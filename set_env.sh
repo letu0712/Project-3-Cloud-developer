6315f985a500c563e8740b2b519144bf9d537db2
letu0712 <letu071299@gmail.com>
2023-05-21 12:23:00 +0700

    UPdate

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=tulx
 export POSTGRES_PASSWORD=tulx123456789
-export POSTGRES_HOST=database-tulx.c7buzgy2ulzb.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-tulx.c832bnfzd66i.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=tulx071299bucket
 export AWS_REGION=us-east-1
