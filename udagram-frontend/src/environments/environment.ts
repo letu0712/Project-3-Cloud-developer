82cbcaf8bf7c6197e82324e8a37008d9f46cc909
letu0712 <letu071299@gmail.com>
2023-05-23 22:37:16 +0700

    Update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aef6855bd3b1d431cbba2a9e2be25f30-2101022708.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
